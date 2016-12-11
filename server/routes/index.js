6e3d00e67120accea61b50106a42e5186825f66b
surajit barman <surajitbarman57@yahoo.in>
2016-12-11 11:13:39 +0530

    added library files to call apis

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var AuthFunctions = require('../lib/auth');
+// var HomeController = require('../controller/home_controller').Home;
+
+var path = require('path');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/*' , function(req,res){
+	res.sendFile(path.resolve('app/views/index.html'))
 });
 
 module.exports = router;
